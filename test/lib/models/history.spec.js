2f6b71835c9b59af96aa43c1ec11b77d68b9fb37
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-12-13 16:29:23 -0500

    History test init

--- test/lib/models/history.spec.js
+++ test/lib/models/history.spec.js
@@ -1,3 +1,10 @@
-/**
- * Created by fbaumgardt on 9/9/16.
- */
+import History from '../../../src/lib/models/history'
+
+describe('#History', function(){
+    let history = new History({model:{}, applicator:{},bar:{navigation:{append:()=>{}}},getEndpoint:function() {return {read:"",write:"",config:""}}})
+    describe('#undo', function(){})
+    describe('#redo', function(){})
+    describe('#add', function(){})
+    describe('#reset', function(){})
+    describe('#commit', function(){})
+})
